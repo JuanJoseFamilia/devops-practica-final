88dd6e05fcae319b2cbc8a0ab92f1557f9e242b4
JuanJoseFamiliaPolanco <familiajuan05@gmail.com>
2025-04-19 09:56:12 -0400

    Mejora prueba unitaria con nombre y matrícula

--- index.test.js
+++ index.test.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-test('La página contiene "Hola Mundo"', () => {
+test('La página contiene ¡Hola Mundo!, el nombre y la matrícula', () => {
     const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
-    expect(html).toMatch(/Hola Mundo/);
+
+    expect(html).toMatch(/¡Hola Mundo!/);
+    expect(html).toMatch(/Juan José Familia/);
+    expect(html).toMatch(/20230218/);
 });
